4bb736a635d92f137c5a4bf8df28581ddbee1e19
Guillaume Danny Deng <zxpay73945788@gmail.com>
2020-12-19 14:52:33 +0800

    add newton_method

---
+++ algorithm/newton_method_equation.jl
@@ -0,0 +1,20 @@
+# Solve the question
+
+function func(x:: Float64) 
+    return x^3 + x^2 -5
+end
+
+
+function newton_iter(x:: Float64)
+    return 
+end
+
+# enfpkewfnmewk
+
+#=
+efml
+qfkmqew
+=#
+
+
+
